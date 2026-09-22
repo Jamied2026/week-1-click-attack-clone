49e96790c1004d4a4a0e540d690b1e6fb7aad025
K00333285: Jamie Donnellan <K00333285@student.tus.ie>
2026-09-22 11:58:39 +0100

    Added functionality to attack and reset buttons

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 let score = 0;
 
+
 const scoreDisplay = document.getElementById("score");
 const title = document.getElementById("title");
 const attackButton = document.getElementById("attackButton");
 const resetButton = document.getElementById("resetButton");
-
 // TODO: create addPoint()
+function addPoint() {
+  score++;
+  updateDisplay();
+}
+
 // TODO: create resetGame()
+function resetGame() {
+  score = 0;
+  title.innerText = "Click Attack";
+  updateDisplay();
+}
+//Update display
+function updateDisplay(){
+  scoreDisplay.innerText = score;
+  if (score >=20) {
+    title.innerText="YOU WIN!"
+  }
+
+
+
+
+
+}
 // TODO: connect both functions to buttons
+attackButton.addEventListener("click", addPoint);
+resetButton.addEventListener("click", resetGame);
